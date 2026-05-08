047e3ffccd617085504527e2829f4594485e49de
nastyuchin07-oss <nastyuchin07@gmail.com>
2026-05-08 21:32:32 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,66 @@
-// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
 
+#include <stdexcept>
+#include <string>
+
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
+ public:
+  TPQueue() : head_(nullptr) {}
+  
+  ~TPQueue() {
+    while (!IsEmpty()) {
+      Pop();
+    }
+  }
+  
+  TPQueue(const TPQueue&) = delete;
+  TPQueue& operator=(const TPQueue&) = delete;
+  
+  void Push(const T& item) {
+    Node* new_node = new Node(item);
+    
+    if (IsEmpty() || item.prior > head_->data.prior) {
+      new_node->next = head_;
+      head_ = new_node;
+      return;
+    }
+    
+    Node* current = head_;
+    while (current->next != nullptr && 
+           current->next->data.prior >= item.prior) {
+      current = current->next;
+    }
+    
+    new_node->next = current->next;
+    current->next = new_node;
+  }
+  
+  T Pop() {
+    if (IsEmpty()) {
+      throw std::runtime_error("Queue is empty");
+    }
+    
+    Node* temp = head_;
+    T result = head_->data;
+    head_ = head_->next;
+    delete temp;
+    return result;
+  }
+  
+  bool IsEmpty() const {
+    return head_ == nullptr;
+  }
 
-struct SYM {
-  char ch;
-  int prior;
+ private:
+  struct Node {
+    T data;
+    Node* next;
+    explicit Node(const T& item) : data(item), next(nullptr) {}
+  };
+  
+  Node* head_;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
